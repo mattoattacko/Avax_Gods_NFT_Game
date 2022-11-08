1731001bffc2234970bdef0a51e93aaa5bac2afd
unknown <matt.mcquain@gmail.com>
2022-11-07 17:58:02 -0800

    added client

---
+++ client/src/components/PageHOC.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { logo, heroImg } from '../assets';
+
+
+const PageHOC = () => {
+  return (
+    <div>PageHOC</div>
+  )
+}
+
+export default PageHOC
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div>
       <h1 className="text-5xl p-3">Avax Gods</h1>
       <h2 className="text-3xl p-3">Web3 NFT Battle-style Card Game</h2>
-      <p className="text-xl p-3">Made with 💜 by JavaScript Mastery</p>
+      <p className="text-xl p-3">Made with 💜 by West Coast Creatives</p>
     </div>
   )
 };
